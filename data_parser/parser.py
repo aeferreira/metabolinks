1d97589c083978117cdc3abcc9626fc907e45d5a
gilpires97 <gilpires071997@gmail.com>
2018-03-18 20:52:45 +0000

    Added printing of combined compound list to parser.py and fixed some errors.

--- data_parser/parser.py
+++ data_parser/parser.py
@@ -9,7 +9,7 @@ def parse4(comps, *args):
                 metadata = comp.split('> ')
                 for item in metadata:
                     if arg in item:
-                        l.append(','.join(item.split('\n')[1:-2]))
+                        l.append(''.join(item.split('\n')[1:2]).replace('CHEBI:', ''))
         data.append(l)
     return data
 # Function: searches for arguments metadata within each compounds data.
@@ -29,9 +29,10 @@ arguments = ['<ChEBI Name>',
              '<HMDB Database Links>',
              '<LIPID MAPS instance Database Links>',
              ]
-order_by_index = 2  # Identify Monoisotopic Mass arg position.
+mass_index = 3  # Identify Mass arg position.
+lipidmaps_index = 7  # Identify Lipid Maps ID arg position.
 chebi_data = parse4(compounds, *arguments)
-chebi_data.sort(key=lambda x: x[order_by_index])
+chebi_data.sort(key=lambda x: x[mass_index])
 
 with open('sample_output_chebi.tsv', 'w') as f:
     # Print header.
@@ -42,6 +43,7 @@ with open('sample_output_chebi.tsv', 'w') as f:
 file_name = 'sample_input_lipidmaps.sdf'
 with open(file_name) as f:
     compounds = f.read().split('$$$$')[:-1]
+# Argumen positions must be the same for ChEBI and Lipid Maps.
 arguments = ['<COMMON_NAME>',
              '<SYSTEMATIC_NAME>',
              '<FORMULA>',
@@ -51,12 +53,51 @@ arguments = ['<COMMON_NAME>',
              '<HMDBID>',
              '<LM_ID>',
              ]
-order_by_index = 2  # Identify Mass arg position.
+mass_index = 3  # Identify Mass arg position.
+lipidmaps_index = 7  # Identify Lipid Maps ID arg position.
 lipidmaps_data = parse4(compounds, *arguments)
-lipidmaps_data.sort(key=lambda x: x[order_by_index])
+lipidmaps_data.sort(key=lambda x: x[mass_index])
 
 with open('sample_output_lipidmaps.tsv', 'w') as f:
     # Print header.
     print('\t'.join(arguments).replace('<', '').replace('>', ''), file=f)
     for compound in lipidmaps_data:
         print('\t'.join(compound), file=f)
+
+# Joining the two data sets. Lipid Maps data prioritized.
+mash_up_data = lipidmaps_data
+temp = [x[lipidmaps_index] for x in lipidmaps_data if x != 'None']
+for comp in chebi_data:
+    if comp[lipidmaps_index] == 'None':
+        mash_up_data.append(comp)
+    elif comp[lipidmaps_index] != 'None' and comp[lipidmaps_index] not in temp:
+        mash_up_data.append(comp)
+        temp.append(comp[7])
+
+# The next segment will:
+# - Convert order_by_index data into float variables.
+# - Order data.
+# - Convert back into strings for 'join' method.
+mash_up_sort = []
+for item in mash_up_data:
+    if item[mass_index] == 'None':
+        item[mass_index] = float(999999.999999)
+        mash_up_sort.append(item)
+    else:
+        item[mass_index] = float(item[mass_index])
+        mash_up_sort.append(item)
+mash_up_sort.sort(key=lambda x: x[mass_index])
+mash_up_data = [map(str, x) for x in mash_up_sort]
+
+with open('sample_output_mash_up.tsv', 'w') as f:
+    # Print header.
+    print("Common Name",
+          "Systematic Name",
+          "Formula",
+          "Monoisotopic Mass",
+          "ChEBI ID",
+          "KEGG Compound ID",
+          "HMBD ID",
+          "Lipid Maps ID", sep='\t', file=f)
+    for item in mash_up_data:
+        print('\t'.join(item), file=f)
