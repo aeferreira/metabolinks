2a984282b4a824b7e94ef69b7bb21e547d545c41
gilpires97 <gilpires071997@gmail.com>
2018-03-21 20:12:02 +0000

    Cleaned up redundant code.

--- data_parser/parser.py
+++ data_parser/parser.py
@@ -1,78 +1,87 @@
-def parse4(comps, *args):
+def parse4(compounds, *args):
     data = []
-    for comp in comps:
+    for c in compounds:
         l = []
         for arg in args:
-            if arg not in comp:
+            if arg not in c:
                 l.append('None')
             else:
-                metadata = comp.split('> ')
-                for item in metadata:
-                    if arg in item:
-                        l.append(''.join(item.split('\n')[1:2]).replace('CHEBI:', ''))
+                metadata = c.split('> ')
+                for i in metadata:
+                    if arg in i:
+                        l.append(''.join(i.split('\n')[1:2]).replace('CHEBI:', ''))
         data.append(l)
     return data
 # Function: searches for arguments metadata within each compounds data.
 # Return: a set of lists, each containing 1 compound's arguments metadata.
-# Input: comps=compounds list; args=arguments/parameters
+# Input: compounds=compounds list; args=arguments/parameters
 
 # ChEBI Parsing.
 file_name = 'sample_input_chebi.sdf'
 with open(file_name) as f:
     compounds = f.read().split('$$$$')[:-1]
-arguments = ['<ChEBI Name>',
-             '<IUPAC Names>',
-             '<Formulae>',
-             '<Monoisotopic Mass>',
-             '<ChEBI ID>',
-             '<KEGG COMPOUND Database Links>',
-             '<HMDB Database Links>',
-             '<LIPID MAPS instance Database Links>',
-             ]
-mass_index = 3  # Identify Mass arg position.
-lipidmaps_index = 7  # Identify Lipid Maps ID arg position.
-chebi_data = parse4(compounds, *arguments)
-chebi_data.sort(key=lambda x: x[mass_index])
-
+args = ['<ChEBI Name>',
+        '<IUPAC Names>',
+        '<Formulae>',
+        '<Monoisotopic Mass>',
+        '<ChEBI ID>',
+        '<KEGG COMPOUND Database Links>',
+        '<HMDB Database Links>',
+        '<LIPID MAPS instance Database Links>'
+        ]
+chebi_data = parse4(compounds, *args)
 with open('sample_output_chebi.tsv', 'w') as f:
     # Print header.
-    print('\t'.join(arguments).replace('<', '').replace('>', ''), file=f)
+    print("Common Name",
+          "Systematic Name",
+          "Formula",
+          "Monoisotopic Mass",
+          "ChEBI ID",
+          "KEGG Compound ID",
+          "HMBD ID",
+          "Lipid Maps ID", sep='\t', file=f)
     for compound in chebi_data:
         print('\t'.join(compound), file=f)
 
+# Lipid Maps Parsing.
 file_name = 'sample_input_lipidmaps.sdf'
 with open(file_name) as f:
     compounds = f.read().split('$$$$')[:-1]
-# Argumen positions must be the same for ChEBI and Lipid Maps.
-arguments = ['<COMMON_NAME>',
-             '<SYSTEMATIC_NAME>',
-             '<FORMULA>',
-             '<EXACT_MASS>',
-             '<CHEBI_ID>',
-             '<KEGG_ID>',
-             '<HMDBID>',
-             '<LM_ID>',
-             ]
+# Equivalent arg positions must be the same for ChEBI and Lipid Maps.
+args = ['<COMMON_NAME>',
+        '<SYSTEMATIC_NAME>',
+        '<FORMULA>',
+        '<EXACT_MASS>',
+        '<CHEBI_ID>',
+        '<KEGG_ID>',
+        '<HMDBID>',
+        '<LM_ID>',
+        ]
 mass_index = 3  # Identify Mass arg position.
 lipidmaps_index = 7  # Identify Lipid Maps ID arg position.
-lipidmaps_data = parse4(compounds, *arguments)
-lipidmaps_data.sort(key=lambda x: x[mass_index])
-
+lipidmaps_data = parse4(compounds, *args)
 with open('sample_output_lipidmaps.tsv', 'w') as f:
     # Print header.
-    print('\t'.join(arguments).replace('<', '').replace('>', ''), file=f)
-    for compound in lipidmaps_data:
-        print('\t'.join(compound), file=f)
+    print("Common Name",
+          "Systematic Name",
+          "Formula",
+          "Monoisotopic Mass",
+          "ChEBI ID",
+          "KEGG Compound ID",
+          "HMBD ID",
+          "Lipid Maps ID", sep='\t', file=f)
+    for lipid in lipidmaps_data:
+        print('\t'.join(lipid), file=f)
 
 # Joining the two data sets. Lipid Maps data prioritized.
 mash_up_data = lipidmaps_data
 temp = [x[lipidmaps_index] for x in lipidmaps_data if x != 'None']
-for comp in chebi_data:
-    if comp[lipidmaps_index] == 'None':
-        mash_up_data.append(comp)
-    elif comp[lipidmaps_index] != 'None' and comp[lipidmaps_index] not in temp:
-        mash_up_data.append(comp)
-        temp.append(comp[7])
+for c in chebi_data:
+    if c[lipidmaps_index] == 'None':
+        mash_up_data.append(c)
+    elif c[lipidmaps_index] != 'None' and c[lipidmaps_index] not in temp:
+        mash_up_data.append(c)
+        temp.append(c[lipidmaps_index])
 
 # The next segment will:
 # - Convert order_by_index data into float variables.
@@ -89,7 +98,7 @@ for item in mash_up_data:
 mash_up_sort.sort(key=lambda x: x[mass_index])
 mash_up_data = [map(str, x) for x in mash_up_sort]
 
-with open('sample_output_mash_up.tsv', 'w') as f:
+with open('sample_final_output.tsv', 'w') as f:
     # Print header.
     print("Common Name",
           "Systematic Name",
