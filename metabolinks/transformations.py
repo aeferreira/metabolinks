211de7fab7b61a26a67e58839e3c6573bfa06e04
aeferreira <ferreiae@gmail.com>
2020-03-29 11:53:30 +0100

    More scaling and normalization methods added.

--- metabolinks/transformations.py
+++ metabolinks/transformations.py
@@ -100,7 +100,99 @@ def normalize_ref_feature(df, feature, remove=True):
         df = df.drop(index=[df.index[pos]])
     return df
 
-# ---------- log transformations and scaling
+def remove_feature(df, feature):
+    """Remove a reference feature (an exact row label).
+
+       df: a Pandas DataFrame.
+       feature: row label.
+
+       Returns: DataFrame.
+       """
+
+    # find position of feature
+    new_index, indexer = df.index.sort_values(return_indexer=True)
+    pos = new_index.get_loc(feature, method='pad')
+    pos = indexer[pos]
+    df = df.drop(index=[df.index[pos]])
+    return df
+
+def normalize_sum (df):
+    """Normalization of a dataset by the total value per columns."""
+
+    return df/df.sum(axis=0)
+
+# Needs double-checking
+def normalize_PQN(df, ref_sample='mean'):
+    """Normalization of a dataset by the Probabilistic Quotient Normalization method.
+
+       df: Pandas DataFrame.
+       ref_sample: reference sample to use in PQ Normalization, types accepted: "mean" (default, reference sample will be the intensity
+    mean of all samples for each feature - useful for when there are a lot of imputed missing values), "median" (reference sample will
+    be the intensity median of all samples for each feature - useful for when there aren't a lot of imputed missing values), sample name
+    (reference sample will be the sample with the given name in the dataset) or list with the intensities of all peaks that will
+    directly be the reference sample (pandas Series format not accepted - list(Series) is accepted).
+
+       Returns: Pandas DataFrame; normalized spectra.
+    """
+    #"Building" the reference sample based on the input given
+    if ref_sample == 'mean': #Mean spectre of all samples
+        ref_sample2 = df.T / df.mean(axis = 1)
+    elif ref_sample == 'median': #Median spectre of all samples
+        ref_sample2 = df.T/df.median(axis = 1)
+    elif ref_sample in df.cdl.samples: #Specified sample of the spectra
+        ref_sample2 = df.T/df.loc[:,ref_sample]
+    else: #Actual sample given
+        ref_sample2 = df.T/ref_sample
+    #Normalization Factor and Normalization
+    Norm_fact = ref_sample2.median(axis=1)
+    return df / Norm_fact
+
+
+def normalize_quantile(df, ref_type='mean'):
+    """Normalization of a dataset by the Quantile Normalization method.
+
+       Missing Values are temporarily replaced with 0 (and count as 0) until normalization is done. Quantile Normalization is more
+    useful with no/low number of missing values.
+
+       Spectra: AlignedSpectra object (from metabolinks).
+       ref_type: str (default: 'mean'); reference sample to use in Quantile Normalization, types accepted: 'mean' (default,
+    reference sample will be the means of the intensities of each rank), 'median' (reference sample will be the medians of the
+    intensities for each rank).
+
+       Returns: AlignedSpectra object (from metabolinks); normalized spectra.
+    """
+    #Setting up the temporary dataset with missing values replaced by zero and dataframes for the results
+    norm = df.copy().replace({np.nan:0})
+    ref_spectra = df.copy()
+    ranks = df.copy()
+
+    for i in range(len(norm)):
+        #Determining the ranks of each entry in the same row (same variable) in the dataset
+        ref_spectra.iloc[i] = norm.iloc[i].sort_values().values
+        ranks.iloc[i] = norm.iloc[i].rank(na_option='top')
+
+    #Determining the reference sample for normalization based on the ref_type chosen (applied on the columns).
+    if ref_type == 'mean':
+        ref_sample = ref_spectra.mean(axis=0).values
+    elif ref_type == 'median':
+        ref_sample = ref_spectra.median(axis=0).values
+    else:
+        raise ValueError('Type not recognized. Available ref_type: "mean", "median".')
+
+    #Replacing the values in the dataset for the reference sample values based on the rankscalculated  earlier for each entry
+    for i in range(len(ranks)):
+        for j in range(len(ranks.columns)):
+            if ranks.iloc[i,j] == round(ranks.iloc[i,j]):
+                norm.iloc[i,j] = ref_sample[int(ranks.iloc[i,j])-1]
+            else: #in case the rank isn't an integer and ends in .5 (happens when a pair number of samples have the same
+                  #value in the same column - after ordering from lowest to highest values by row).
+                norm.iloc[i,j] = np.mean((ref_sample[int(ranks.iloc[i,j]-1.5)], ref_sample[int(ranks.iloc[i,j]-0.5)]))
+
+    #Replacing 0's by missing values and creating the AlignedSpectra object for the output
+    return norm.replace({0:np.nan})
+
+
+# ---------- log transformations and scalings
 
 def glog(df, lamb=None):
     """Performs Generalized Logarithmic Transformation on a Spectra (same as MetaboAnalyst's transformation).
@@ -126,6 +218,83 @@ def pareto_scale(df):
     stds = df.std(axis=1) ** 0.5
     df2 = df.sub(means, axis=0).div(stds, axis=0)
     return df2
+
+
+def mean_center(df):
+    """Performs Mean Centering.
+
+       df: Pandas DataFrame. It can include missing values.
+
+       Returns: DataFrame; Mean Centered Spectra."""
+    return df.sub(df.mean(axis=1), axis=0)
+
+
+def auto_scale(df):
+    """Performs Autoscaling on column-organized data.
+
+       Returns: Pandas DataFrame; Auto Scaled Spectra.
+
+       This is x -> (x - mean(x)) / std(x) per feature"""
+
+    # TODO: verify if the name of this transformation is "Standard scaling"
+    # TODO: most likely it is known by many names (scikit-learn has a SatndardScaler transformer)
+    means = df.mean(axis=1)
+    stds = df.std(axis=1)
+    df2 = df.sub(means, axis=0).div(stds, axis=0)
+    return df2
+
+def range_scale(df):
+    """Performs Range Scaling.
+
+       Returns: Pandas DataFrame."""
+
+    scaled_aligned = df.copy()
+    ranges = df.max(axis=1) - df.min(axis=1) # Defining range for every feature
+    # Applying Range scaling to each feature
+    for j in range(0, len(scaled_aligned)):
+        if ranges.iloc[j] == 0: # No difference between max and min values
+            scaled_aligned.iloc[j, :] = df.iloc[j, ]
+        else:
+            scaled_aligned.iloc[j, :] = (df.iloc[j, ] - df.iloc[j, ].mean()) / ranges.iloc[j]
+
+    return scaled_aligned
+
+
+def vast_scale(df):
+    """Performs Vast Scaling.
+
+       Returns: Pandas DataFrame; Vast Scaled Spectra."""
+
+    # scaled_aligned = df.copy()
+    std = df.std(axis=1)
+    mean = df.mean(axis=1)
+    # Applying Vast Scaling to each feature
+    scaled_aligned = (((df.T - mean)/std)/(mean/std)).T
+
+    # Return scaled spectra
+    return scaled_aligned
+
+
+def level_scale(df, average=True):
+    """Performs Level Scaling on a DataFrame. (See van den Berg et al., 2006).
+
+    average: bool (Default - True); if True mean-centered data is divided by the mean spectra, if False it is divided by the median
+    spectra.
+
+    Returns: Pandas DataFrame; Level Scaled Spectra."""
+
+    mean = df.mean(axis=1)
+    # Applying Level Scaling to each feature
+    if average == True:
+        scaled_aligned = ((df.T - mean)/mean).T
+    elif average == False:
+        scaled_aligned = ((df.T - mean)/df.median(axis=1)).T
+    else:
+        raise ValueError ('Average is a boolean argument. Only True or False accepted.')
+
+    # Return scaled spectra
+    return scaled_aligned
+
 
 # ----------------------------------------
 # MassTRIX related functions
