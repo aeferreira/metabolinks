fd43d7d1270e1a3a6c9734fc223ff8ac6a087a9d
aeferreira <ferreiae@gmail.com>
2020-07-07 18:10:36 +0100

    Major modifications in the demo datasets code. Still unfinished (tests are broken, now).

--- metabolinks/datasets.py
+++ metabolinks/datasets.py
@@ -1,15 +1,80 @@
 """Example data sets.
 
-   For now, they are used essencially to testing purposes."""
-
+   For now, they are used essencially for testing purposes."""
+from abc import ABCMeta, abstractmethod
 import pandas as pd
 import six
 
-# def load_dataset(name, as_str=False):
-#     pass
+class DataSetBase(metaclass=ABCMeta):
+    """ Base class for an example data set"""
 
-def demo_data1():
-    return """m/z	s38	s39	s40	s32	s33	s34
+    def __init__(self, **kwargs):
+        """ Constructor """
+        pass
+
+    @abstractmethod
+    def as_dataframe(self):
+        """ Abstract method to return the dataset as pandas DataFrame."""
+        pass
+
+    @abstractmethod
+    def as_str(self):
+        """ Abstract method to return the dataset as a string."""
+        pass
+
+    @abstractmethod
+    def create_example_file(self, file_name):
+        """ Abstract method to create a file with the dataset of appropriate type."""
+        pass
+
+class DataSetFactory:
+    """ The factory class for creating example data sets"""
+
+    registry = {}
+    """ Internal registry for available datasets"""
+
+    @classmethod
+    def register(cls, name):
+        """ Class method to register a data set to the internal registry.
+        Args:
+            name (str): The name of the data set.
+        Returns:
+            The data set class itself.
+        """
+
+        def inner_wrapper(wrapped_class):
+            # if already exists: replaces entry
+            cls.registry[name] = wrapped_class
+            return wrapped_class
+
+        return inner_wrapper
+
+    @classmethod
+    def create_dataset(cls, name, **kwargs):
+        """ Factory command to create the data set.
+        This method gets the appropriate DataSet class from the registry
+        and creates an instance of it, while passing in the parameters
+        given in ``kwargs``.
+        Args:
+            name (str): The name of the data set to create.
+        Returns:
+            An instance of the data set that is created.
+        """
+
+        if name not in cls.registry:
+            raise AttributeError('Data set {} is not registered'.format(name))
+
+
+        dataset_class = cls.registry[name]
+        dataset = dataset_class(**kwargs)
+        return dataset
+
+
+@DataSetFactory.register('demo1')
+class Demo1(DataSetBase):
+
+    def as_str(self):
+        return """m/z	s38	s39	s40	s32	s33	s34
 97.58868	1073218	1049440	1058971	2351567	1909877	2197036
 97.59001	637830	534900	582966	1440216	1124346	1421899
 97.59185	460092	486631		1137139	926038	1176756
@@ -30,9 +95,20 @@ def demo_data1():
 98.57899		1877649	1864650	1573559		1829208
 99.28772	2038979				3476845	"""
 
-def demo_data2():
-    return """label	l1	l2	l1	l2	l2	l2
+    def as_dataframe(self):
+        return pd.read_csv(six.StringIO(self.as_str()), sep='\t', index_col=0)
+
+    def create_example_file(self, file_name):
+        df = self.as_dataframe()
+        df.to_csv(file_name)
+
+@DataSetFactory.register('demo2')
+class Demo2(DataSetBase):
+
+    def as_str(self):
+        return """label	l1	l2	l1	l2	l2	l2
 sample	s38	s39	s40	s32	s33	s34
+m/z
 97.58868	1073218	1049440	1058971	2351567	1909877	2197036
 97.59001	637830	534900	582966	1440216	1124346	1421899
 97.59185	460092	486631		1137139	926038	1176756
@@ -53,8 +129,20 @@ sample	s38	s39	s40	s32	s33	s34
 98.57899		1877649	1864650	1573559		1829208
 99.28772	2038979				3476845	"""
 
-def table_with_formulae():
-    return pd.read_csv(six.StringIO("""Name	formula	Neutral mass
+
+    def as_dataframe(self):
+        return pd.read_csv(six.StringIO(self.as_str()), sep='\t', index_col=0, header=[0,1])
+
+    def create_example_file(self, file_name):
+        df = self.as_dataframe()
+        df.to_csv(file_name)
+
+
+@DataSetFactory.register('table_with_formulae')
+class TableWithFormulae(DataSetBase):
+
+    def as_str(self):
+        return """Name	formula	Neutral mass
 (+)-(1R,2R)-1,2-Diphenylethane-1,2-diol ([M+H]+)	C14H14O2	215.1066562
 (+)-(1R,2R)-1,2-Diphenylethane-1,2-diol (see KEGG C16015) [secondary alcohol] ([M+H]+)	C14H14O2	215.1066562
 (+)-10-methyl lauric acid; 10-methyl-dodecanoic acid [Branched fatty acids [FA0102]] ([M+H]+)	C13H26O2	215.2005565
@@ -83,11 +171,20 @@ def table_with_formulae():
 (+)-8-Daucen-5-ol [C15 isoprenoids (sesquiterpenes) [PR0103]] ([M+H]+)	C15H26O	223.2056419
 (+)-8-Drimen-7-one [C15 isoprenoids (sesquiterpenes) [PR0103]] ([M+H]+)	C15H24O	221.1899919
 (+)-9,10,18-trihydroxy-12Z-octadecenoic acid [Hydroxy fatty acids [FA0105]] ([M+K39]+)	C18H34O5	369.2037836
-(+)-Acutifolin A [Flavans, Flavanols and Leucoanthocyanidins [PK1202]] ([M+H]+)	C20H22O4	327.1590857
-"""), sep='\t')
+(+)-Acutifolin A [Flavans, Flavanols and Leucoanthocyanidins [PK1202]] ([M+H]+)	C20H22O4	327.1590857"""
 
-def MassTRIX_output():
-    return """133.08577	0.00E+00	132.0784935	26	133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401	-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#	C03264#C03499#C06103#C07834#HMDB00317#HMDB00409#HMDB00525#HMDB00624#HMDB00665#HMDB00746#HMDB01624#HMDB01975#HMDB10718#HMDB12843#LMFA01050011#LMFA01050012#LMFA01050013#LMFA01050014#LMFA01050015#LMFA01050376#LMFA01050377#LMFA01050380#LMFA01050381#LMFA01050387#LMFA01050402#LMFA01050408	C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3	"D-2-Hydroxyisocaproate ([M+H]+)#Ethyl (R)-3-hydroxybutanoate ([M+H]+)#6-Hydroxyhexanoic acid;6-Hydroxyhexanoate ([M+H]+)#Paraldehyde ([M+H]+)#2-Hydroxy-3-methylpentanoic acid; (2R,3R)-2-hydroxy-3-methyl-pentanoic acid [secondary alcohol] ([M+H]+)#(5R)-5-Hydroxyhexanoic acid; (5R)-5-hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#5-Hydroxyhexanoic acid; 5-hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#D-Leucic acid (see KEGG C03264); (2R)-2-hydroxy-4-methylpentanoic acid [secondary alcohol] ([M+H]+)#Leucinic acid; 2-hydroxy-4-methylpentanoic acid [secondary alcohol] ([M+H]+)#Hydroxyisocaproic acid; (2S)-2-hydroxy-4-methylpentanoic acid [secondary alcohol] ([M+H]+)#2-Hydroxycaproic acid; 2-hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#2-Ethyl-2-Hydroxybutyric acid; 2-ethyl-2-hydroxy-butanoic acid [tertiary alcohol] ([M+H]+)#(R)-3-Hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#6-Hydroxyhexanoic acid (see KEGG C06103); 6-hydroxyhexanoate [primary alcohol] ([M+H]+)#DL-2-hydroxy caproic acid; 2-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#DL-3-hydroxy caproic acid; 3-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#DL-4-hydroxy caproic acid; 4-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#5-hydroxy caproic acid; 5-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#6-hydroxy caproic acid; 6-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#5R-hydroxy-hexanoic acid; 5R-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#3R-hydroxy-hexanoic acid; 3R-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#2-hydroxy-3-methyl-pentanoic acid; 2R-hydroxy-3R-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#Leucinic acid; 2-hydroxy-4-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#2-ethyl-2-hydroxy-butyric acid; 2-ethyl-2-hydroxy-butanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#D-Leucic acid; 2R-hydroxy-4-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#hydroxy-isocaproic acid; 2S-hydroxy-4-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)"													null#null#ko00930#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null	"null#null#;Caprolactam degradation#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null"	null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null
+    def as_dataframe(self):
+        return pd.read_csv(six.StringIO(self.as_str()), sep='\t', index_col=None).set_index('Name')
+
+    def create_example_file(self, file_name):
+        df = self.as_dataframe()
+        df.to_csv(file_name)
+
+@DataSetFactory.register('masstrix_output')
+class MasstrixOutput(DataSetBase):
+
+    def as_str(self):
+        return """133.08577	0.00E+00	132.0784935	26	133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401#133.085920710401	-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#-1.1324306198618#	C03264#C03499#C06103#C07834#HMDB00317#HMDB00409#HMDB00525#HMDB00624#HMDB00665#HMDB00746#HMDB01624#HMDB01975#HMDB10718#HMDB12843#LMFA01050011#LMFA01050012#LMFA01050013#LMFA01050014#LMFA01050015#LMFA01050376#LMFA01050377#LMFA01050380#LMFA01050381#LMFA01050387#LMFA01050402#LMFA01050408	C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3#C6H12O3	"D-2-Hydroxyisocaproate ([M+H]+)#Ethyl (R)-3-hydroxybutanoate ([M+H]+)#6-Hydroxyhexanoic acid;6-Hydroxyhexanoate ([M+H]+)#Paraldehyde ([M+H]+)#2-Hydroxy-3-methylpentanoic acid; (2R,3R)-2-hydroxy-3-methyl-pentanoic acid [secondary alcohol] ([M+H]+)#(5R)-5-Hydroxyhexanoic acid; (5R)-5-hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#5-Hydroxyhexanoic acid; 5-hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#D-Leucic acid (see KEGG C03264); (2R)-2-hydroxy-4-methylpentanoic acid [secondary alcohol] ([M+H]+)#Leucinic acid; 2-hydroxy-4-methylpentanoic acid [secondary alcohol] ([M+H]+)#Hydroxyisocaproic acid; (2S)-2-hydroxy-4-methylpentanoic acid [secondary alcohol] ([M+H]+)#2-Hydroxycaproic acid; 2-hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#2-Ethyl-2-Hydroxybutyric acid; 2-ethyl-2-hydroxy-butanoic acid [tertiary alcohol] ([M+H]+)#(R)-3-Hydroxyhexanoic acid [secondary alcohol] ([M+H]+)#6-Hydroxyhexanoic acid (see KEGG C06103); 6-hydroxyhexanoate [primary alcohol] ([M+H]+)#DL-2-hydroxy caproic acid; 2-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#DL-3-hydroxy caproic acid; 3-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#DL-4-hydroxy caproic acid; 4-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#5-hydroxy caproic acid; 5-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#6-hydroxy caproic acid; 6-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#5R-hydroxy-hexanoic acid; 5R-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#3R-hydroxy-hexanoic acid; 3R-hydroxy-hexanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#2-hydroxy-3-methyl-pentanoic acid; 2R-hydroxy-3R-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#Leucinic acid; 2-hydroxy-4-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#2-ethyl-2-hydroxy-butyric acid; 2-ethyl-2-hydroxy-butanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#D-Leucic acid; 2R-hydroxy-4-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)#hydroxy-isocaproic acid; 2S-hydroxy-4-methyl-pentanoic acid [Hydroxy fatty acids [FA0105]] ([M+H]+)"													null#null#ko00930#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null	"null#null#;Caprolactam degradation#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null"	null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null#null
 149.01145	0.00E+00	110.0482906	3	149.011172241631#149.011172241631#149.011172241631	1.86400688677955#1.86400688677955#1.86400688677955	C02560#C05130#HMDB03905	C5H6N2O#C5H6N2O#C5H6N2O	"N-Acetylimidazole ([M+K39]+)#Imidazole-4-acetaldehyde;Imidazole acetaldehyde ([M+K39]+)#Imidazole-4-acetaldehyde (see KEGG C05130); 2-(3H-imidazol-4-yl)acetaldehyde [aldehyde] ([M+K39]+)"													null#ko00340#null	"null#;Histidine metabolism#null"	null#null#null
 177.00634	0.00E+00	138.0431806	4	177.006086861191#177.006086861191#177.006086861191#177.006086861191	1.43011153725101#1.43011153725101#1.43011153725101#1.43011153725101	C00785#C02126#HMDB00301#HMDB02730	C6H6N2O2#C6H6N2O2#C6H6N2O2#C6H6N2O2	"Urocanate;Urocanic acid ([M+K39]+)#4-Nitroaniline;p-Nitroaniline;4-Nitrobenzeneamine ([M+K39]+)#Urocanic acid (see KEGG C00785); 3-(3H-imidazol-4-yl)prop-2-enoic acid [carboxylic acid] ([M+K39]+)#Nicotinamide N-oxide; 1-oxidopyridin-1-ium-3-carboxamide [N-oxide] ([M+K39]+)"													"ko00340;ko01100#null#null#null"	";Histidine metabolism;Metabolic pathways#null#null#null"	null#null#null#null
 189.04265	0.00E+00	150.0794906	1	189.0424724	0.939629703	C17512	C8H10N2O	"N-Methylanthranilamide;N-Methyl-2-aminobenzamide ([M+K39]+)"													null	null	null
@@ -104,3 +201,55 @@ def MassTRIX_output():
 249.18444	0.00E+00	248.1771635	13	249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681#249.184906475681	-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677#-1.87200966885677	LMFA01030163#LMFA01030164#LMFA01030165#LMFA01030166#LMFA01030277#LMFA01030278#LMFA01030279#LMFA01030280#LMFA01030491#LMFA01030492#LMFA01030493#LMFA01030870#LMFA01140010	C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2#C16H24O2	"16:4(4Z,7Z,10Z,13Z); 4,7,10,13-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#4,7,11,14-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#4,8,12,16-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#16:4(6Z,9Z,12Z,15Z); 6,9,12,15-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#2E,6Z,8Z,12E-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#2E,6Z,8Z,12Z-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#2Z,6Z,8Z,12E-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#2Z,6Z,8Z,12Z-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#3,9-hexadecadiynoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#7,10-hexadecadiynoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#8,10-hexadecadiynoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#16:4(6Z,9Z,12Z,15Z); 6Z,9Z,12Z,15Z-hexadecatetraenoic acid [Unsaturated fatty acids [FA0103]] ([M+H]+)#4-[3]-ladderane-butanoic acid [Carbocyclic fatty acids [FA0114]] ([M+H]+)"													null#null#null#null#null#null#null#null#null#null#null#null#null	null#null#null#null#null#null#null#null#null#null#null#null#null	null#null#null#null#null#null#null#null#null#null#null#null#null
 raw_mass	peak_height	corrected_mass	npossible	KEGG_mass	ppm	KEGG_cid	KEGG_formula	KEGG_name	uniqueID	C13	O18	N15	S34	Mg25	Mg26	Fe54	Fe57	Ca44	Cl37	K41	KEGG Pathways	KEGG Pathways descriptions	Compound in Organism(X)"""
 
+    def as_dataframe(self):
+        df = pd.read_csv(six.StringIO(self.as_str()), sep='\t', index_col=None, header=None)
+        df.columns = list(df.iloc[-1])
+        df = df.iloc[0:-1, :]
+        return df.set_index(df.columns[0])
+
+    def create_example_file(self, file_name):
+        df = self.as_dataframe()
+        df.to_csv(file_name)
+
+
+def demo_dataset(name):
+    return DataSetFactory.create_dataset(name)
+
+
+def demo(name):
+    return DataSetFactory.create_dataset(name).as_dataframe()
+
+
+if __name__ == '__main__':
+    d1 = demo_dataset('demo1')
+    print(d1.as_str())
+    print(d1.as_dataframe())
+    print('-'*40)
+    d1 = demo('demo1')
+    print(d1)
+    print('-'*40)
+    d2 = demo_dataset('demo2')
+    print(d2)
+    print(d2.as_str())
+    print(d2.as_dataframe())
+    print('-'*40)
+    d2 = demo('demo2')
+    print(d2)
+    print(d2.columns.names)
+    print(d2.index.names)
+    print('-'*40)
+    d2 = demo_dataset('table_with_formulae')
+    print(d2.as_str())
+    print(d2.as_dataframe())
+    print('-'*40)
+    d2 = demo('table_with_formulae')
+    print(d2.columns.names)
+    print(d2.index.names)
+    print('-'*40)
+    d2 = demo_dataset('masstrix_output')
+    print(d2.as_str())
+    print(d2.as_dataframe())
+    print('-'*40)
+    d2 = demo('masstrix_output')
+    print(d2.columns.names)
+    print(d2.index.names)
